e1499c196938866cb814f5f815eccef981e93185
exkuretrol <33695301+exkuretrol@users.noreply.github.com>
2024-06-22 09:14:19 +0800

    fix: use fixed secret key to prevent session corrupt

--- django_project/settings.py
+++ django_project/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
-# import secret key generator from django
-from django.core.management.utils import get_random_secret_key
 from environs import Env
 
 env = Env()
@@ -27,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_random_secret_key()
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)

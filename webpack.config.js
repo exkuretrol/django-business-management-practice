d69b9cfc1a3ea64a266d967824166f182628691d
exkuretrol <33695301+exkuretrol@users.noreply.github.com>
2024-06-23 16:42:03 +0800

    chore: split source javascript and webpack javascript to make code cleaner

--- webpack.config.js
+++ webpack.config.js
@@ -3,58 +3,68 @@ const path = require("path");
 const webpack = require("webpack");
 const autoprefixer = require("autoprefixer");
 const bundletracker = require("webpack-bundle-tracker");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-    mode: "development",
-    entry: "./assets/js/index.js",
-    output: {
-        filename: "[name]-[contenthash].js",
-        path: path.resolve(__dirname, "assets", "webpack_bundles"),
-        publicPath: "auto",
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery",
-        }),
-        new bundletracker({ path: __dirname, filename: "webpack-stats.json" }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: "asset/resource",
-            },
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    {
-                        loader: "style-loader",
-                    },
-                    {
-                        loader: "css-loader",
-                    },
-                    {
-                        loader: "postcss-loader",
-                        options: {
-                            postcssOptions: {
-                                plugins: [autoprefixer],
+module.exports = (env, argv) => {
+    const mode = argv.mode || "development";
+    const dev_mode = mode === "development";
+
+    const config = {
+        entry: "./assets/src/js/index.js",
+        output: {
+            filename: "[name]-[contenthash].js",
+            path: path.resolve(__dirname, "assets", "webpack_bundles"),
+            publicPath: "auto",
+            clean: true,
+        },
+        plugins: [
+            new webpack.ProvidePlugin({
+                $: "jquery",
+                jQuery: "jquery",
+            }),
+            new bundletracker({
+                path: __dirname,
+                filename: "webpack-stats.json",
+            }),
+            new MiniCssExtractPlugin({
+                filename: "[name].css",
+                chunkFilename: "[id].css",
+            }),
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                    type: "asset/resource",
+                },
+                {
+                    test: /\.(sa|sc|c)ss$/,
+                    use: [
+                        {
+                            loader: dev_mode
+                                ? "style-loader"
+                                : MiniCssExtractPlugin.loader,
+                        },
+                        {
+                            loader: "css-loader",
+                        },
+                        {
+                            loader: "postcss-loader",
+                            options: {
+                                postcssOptions: {
+                                    plugins: [autoprefixer],
+                                },
                             },
                         },
-                    },
-                    {
-                        loader: "sass-loader",
-                    },
-                ],
-            },
-            // {
-            //     test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            //     type: "asset/resource",
-            // },
-        ],
-    },
+                        {
+                            loader: "sass-loader",
+                        },
+                    ],
+                },
+            ],
+        },
+        devtool:
+            mode === "development" ? "cheap-module-eval-source-map" : false,
+    };
+    return config;
 };

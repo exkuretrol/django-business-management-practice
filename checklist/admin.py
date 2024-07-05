b1f3e33c81c197d6af0e410b0f24eb196942b49b
exkuretrol <33695301+exkuretrol@users.noreply.github.com>
2024-07-06 04:10:51 +0800

    feat: replace integer primary key with uuid4 in checklist model

--- checklist/admin.py
+++ checklist/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from .models import Checklist, ChecklistTemplate
 
@@ -6,26 +7,40 @@ from .models import Checklist, ChecklistTemplate
 @admin.register(Checklist)
 class ChecklistAdmin(admin.ModelAdmin):
     list_display = [
+        "template_id",
         "branch",
-        "content",
         "status",
         "order",
-        "priority",
+        "get_priority",
         "effective_start_date",
         "effective_end_date",
         "created_at",
         "last_modified",
+        "last_modified_by",
     ]
-    list_filter = ["status", "priority"]
+
+    @admin.display(description=_("優先度"), ordering="template_id__priority")
+    def get_priority(self, obj):
+        return obj.template_id.priority
+
+    def get_effective_end_date(self, obj):
+        return obj.template_id.effective_end_date
+
+    list_filter = ["status", "template_id__priority"]
     search_fields = ["branch__name", "content"]
 
 
 @admin.register(ChecklistTemplate)
 class ChecklistTemplateAdmin(admin.ModelAdmin):
     list_display = [
+        # "branchs",
         "content",
         "order",
+        "priority",
         "effective_start_date",
         "effective_end_date",
+        "last_modified",
+        "last_modified_by",
     ]
+
     search_fields = ["content"]
--- checklist/models.py
+++ checklist/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
@@ -8,23 +10,27 @@ def now_plus_7_days() -> timezone.datetime:
 
 
 class PriorityChoices(models.IntegerChoices):
-    URGENT = 1, _("緊急")
+    ABNORMAL = 1, _("異常")
     TEMPORARY = 2, _("臨時")
     ROUTINE = 3, _("例行")
 
 
 class Checklist(models.Model):
+    uuid = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False, verbose_name=_("UUID")
+    )
+    template_id = models.ForeignKey(
+        to="checklist.ChecklistTemplate",
+        verbose_name=_("待做清單模板 ID"),
+        on_delete=models.CASCADE,
+    )
     branch = models.ForeignKey(
         to="branch.Branch",
         verbose_name=_("門市"),
         on_delete=models.CASCADE,
     )
-    content = models.CharField(verbose_name=_("內容"), max_length=255)
     status = models.BooleanField(verbose_name=_("狀態"), default=False)
     order = models.IntegerField(verbose_name=_("排序"), default=0)
-    priority = models.IntegerField(
-        verbose_name=_("優先度"), default=0, choices=PriorityChoices.choices
-    )
     effective_start_date = models.DateField(
         verbose_name=_("生效起日"), default=timezone.now
     )
@@ -33,9 +39,16 @@ class Checklist(models.Model):
     )
     created_at = models.DateTimeField(verbose_name=_("建立時間"), auto_now_add=True)
     last_modified = models.DateTimeField(verbose_name=_("最後更新時間"), auto_now=True)
+    last_modified_by = models.ForeignKey(
+        verbose_name=_("最後更新者"),
+        to="core.User",
+        on_delete=models.SET_NULL,
+        null=True,
+        editable=False,
+    )
 
     def __str__(self) -> str:
-        return f"{self.branch} - {self.content}"
+        return f"{self.branch} - {self.template_id.content}"
 
 
 class ChecklistTemplate(models.Model):
@@ -44,9 +57,25 @@ class ChecklistTemplate(models.Model):
     )
     content = models.TextField(verbose_name=_("內容"))
     order = models.IntegerField(verbose_name=_("排序"), default=0)
+    priority = models.IntegerField(
+        verbose_name=_("優先度"),
+        default=0,
+        choices=PriorityChoices.choices,
+    )
     effective_start_date = models.DateField(
         verbose_name=_("生效起日"), default=timezone.now
     )
     effective_end_date = models.DateField(
         verbose_name=_("生效迄日"), default=now_plus_7_days
     )
+    last_modified = models.DateTimeField(verbose_name=_("最後更新時間"), auto_now=True)
+    last_modified_by = models.ForeignKey(
+        verbose_name=_("最後更新者"),
+        to="core.User",
+        on_delete=models.SET_NULL,
+        null=True,
+        editable=False,
+    )
+
+    def __str__(self) -> str:
+        return f"{self.pk} - ({PriorityChoices(self.priority).label}) {self.content}"

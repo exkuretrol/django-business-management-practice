0674c0e54257afc6b8c4c753e485679e20e6e49d
exkuretrol <33695301+exkuretrol@users.noreply.github.com>
2024-07-06 04:11:14 +0800

    feat: update generate routine checklist script

--- checklist/jobs/daily/generate_routine_checklist.py
+++ checklist/jobs/daily/generate_routine_checklist.py
@@ -10,40 +10,43 @@ class Job(DailyJob):
     help = _("產生例行待做清單")
 
     def execute(self):
+        # start date and end date will be today
+        start_date = timezone.localdate()
+        end_date = start_date
+
+        # filter valid checklist template
         checklist_valid = ChecklistTemplate.objects.filter(
-            effective_start_date__lte=timezone.now(),
-            effective_end_date__gte=timezone.now(),
+            effective_start_date__lte=start_date,
+            effective_end_date__gte=end_date,
+            priority=PriorityChoices.ROUTINE,
         )
 
-        # create checklist for all branchs
+        # filter checklist for all branchs
         checklist_for_all = checklist_valid.filter(branchs__isnull=True)
-        Checklist.objects.bulk_create(
-            [
-                Checklist(
-                    branch=branch,
-                    content=template.content,
-                    priority=PriorityChoices.ROUTINE,
-                    order=template.order,
-                    effective_start_date=template.effective_start_date,
-                    effective_end_date=template.effective_end_date,
-                )
-                for branch in Branch.objects.all()
-                for template in checklist_for_all
-            ]
-        )
+        checklist_objects_list = []
+        checklist_objects_list += [
+            Checklist(
+                template_id=template,
+                branch=branch,
+                effective_start_date=start_date,
+                effective_end_date=end_date,
+            )
+            for branch in Branch.objects.all()
+            for template in checklist_for_all
+        ]
 
-        # create checklist for specific branchs
+        # filter checklist for specific branchs
         checklist_specific_branchs = checklist_valid.exclude(branchs__isnull=True)
-        Checklist.objects.bulk_create(
-            [
-                Checklist(
-                    branch=branch,
-                    content=template.content,
-                    priority=PriorityChoices.ROUTINE,
-                    effective_start_date=template.effective_start_date,
-                    effective_end_date=template.effective_end_date,
-                )
-                for template in checklist_specific_branchs
-                for branch in template.branchs.all()
-            ]
-        )
+        checklist_objects_list += [
+            Checklist(
+                template_id=template,
+                branch=branch,
+                effective_start_date=start_date,
+                effective_end_date=end_date,
+            )
+            for template in checklist_specific_branchs
+            for branch in template.branchs.all()
+        ]
+
+        # bulk create checklist
+        Checklist.objects.bulk_create(checklist_objects_list)
